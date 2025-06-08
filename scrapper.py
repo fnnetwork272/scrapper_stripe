2f6c44eb900a16e89baf78ac1853c73fe305062a
fnnetwork272 <fnnetwork272@gmail.com>
2025-06-08 08:37:24 +0530

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -17,7 +17,7 @@ api_id = 21568121  # Replace with your actual API ID
 api_hash = "372a11990ed6beff0ce00b5d2bc2d5ef"  # Replace with your actual API hash
 session_name = "cc_scraper"
 # Add your session string here (optional); leave as None if using session file or manual login
-session_string = "1BVtsOKEBuyTS5d6yFrw9vyY0_QeGrf7ZUhndU4VIZZMh2OJjGDgtakzUPiTeK1z5_-ET55wEMfr0NEgsV_AE4gGN6XIu6dFYpwuaN-3TOsltsxflnXMNVBdY4teWqJP4FyxqrAcFzYHUqMpRPFRwcCIoatLiXS97KK6priQK79o-S1ZQVVOcPkKcpPTvu4FZBAYq1274LiW29mK8ne9GCynTNVTCrV6vTpkVLNgWKTfEsoJSmgkrUE8O14ZpvdCSWPaThZCvBsVvi2AIez0IdjhFobo7rrnja20BCKJX4E5ouMRXxSngwp-bMKrQ9uLUy5aA-R2HVZF3733sbNzXQZH8NRfI9tA="  # Replace with your session string, e.g., "1BVtsO..."
+session_string = "1BVtsOKEBu5wkKv9YTVUBhcunap5Po6Qq0K8rEeTZsOEoCjq0YYhJtb-Njtsy4qPa9P5AeRvmhCFvSPJ0jtb00aJk6VamdJXdL-jJAEchiPX71xsOUvUaYuoUw2IFJgoJ7r3yfpjbu8S_RVoad1Ihwasl5FvyhhUkBNPw2VpZtDq-4HN1dQKtoA5GwpimZVAFIdtGUgAiaTwfeRiSOq4RKNlBWG3nlBHXVmB61kCm144o5L1L8bV6k8X61jmNZdHtvlLsKzSHCFyuYf7n74l9iyzX-4ZIa6qJTfcWOz9A6kuhfS2etQe045ik2WVjLQOrnzwbmxDKIqFl1DmelX1CDE3gEKEWuOk="  # Replace with your session string, e.g., "1BVtsO..."
 
 # Sources Configuration - add as many as needed
 source_groups = [-1002682944548, -1001878543352]  # Add source group IDs if needed
